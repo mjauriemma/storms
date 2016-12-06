f9e0bd46101a323a29343786d112fa768dd3984a
mjauriemma <matthewauriemma@gmail.com>
2016-12-06 12:30:55 -0600

    adding post

--- controllers/search.js
+++ controllers/search.js
@@ -37,6 +37,34 @@ router.get('/', (req, res) => {
 
 });
 
+router.post('/', (req, res) => {
+    let lat = req.query.lat;
+    let long = req.query.long;
+    //let latmax = req.query.latmax;
+    //let longmax = req.query.longmax;
+    let yearmin = req.query.yearmin;
+    let yearmax = req.query.yearmax;
+    //if (!latmin || !longmin || !latmax || !longmax) {
+    if (!lat || !long){
+        return webUtil.processError(res, 400, 'Latitude and Longitude must be provided', 400);
+    }
+    if (!yearmin || yearmin < 1945) {
+      yearmin = 1945;
+    }
+    if (!yearmax || yearmax > 2016) {
+      yearmax = 2016;
+    }
+      storms.searchAsync(lat, long, yearmin, yearmax)
+          .then(response => {
+              return res.send(response);
+          })
+          .catch(err => {
+            console.log(err);
+              return webUtil.processError(res, 500, err.message);
+          });
+
+});
+
 
 
 
