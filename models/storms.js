259f54c9ae3406fec8ddb12de937223bb1721e56
mjauriemma <matthewauriemma@gmail.com>
2016-12-06 09:12:19 -0600

    Db Test Push

--- models/storms.js
+++ models/storms.js
@@ -10,25 +10,30 @@ let Promise = require('bluebird');
 var dbFactory = require('./db');
 var db = Promise.promisifyAll(dbFactory(config.db));
 // let schema = require('./schema').tweets;
+var selStorms = "Select * from `TyphoonX` where LAT > (? - ?) and" +
+                "LAT < (? + ?) and " +
+                "LONG > (? - ?) and LONG < (? + ?)"
 
 
-function search(latmin, longmin, latmax, longmax, yearmin, yearmax, callback) {
+function search(lat, long, yearmin, yearmax, callback) {
 
-    if (latmin < -90 || latmin > 90 ||latmax <-90 || latmax > 90) {
-      console.log("\tLatitude: " + latmin);
-      console.log("\tLatitude: " + latmax);
-      console.log("\tLongitude: " + longmin);
-      console.log("\tLongitude: " + longmax);
+    //if (latmin < -90 || latmin > 90 ||latmax <-90 || latmax > 90) {
+    if (lat < -90 || lat > 90) {
+      //console.log("\tLatitude: " + latmin);
+      console.log("\tLatitude: " + lat);
+      //console.log("\tLongitude: " + longmin);
+      console.log("\tLongitude: " + long);
       console.log("\tYear: " + yearmin);
       console.log("\tYear: " + yearmax);
       console.log("\tError: Invalid Latitude");
       return callback (new bi("Invalid Latitude"));
     }
-    else if (longmin < -180 || longmin > 180 || longmax < -180 || longmax > 180) {
-      console.log("\tLatitude: " + latmin);
-      console.log("\tLatitude: " + latmax);
-      console.log("\tLongitude: " + longmin);
-      console.log("\tLongitude: " + longmax);
+    // else if (longmin < -180 || longmin > 180 || longmax < -180 || longmax > 180) {
+    else if (long < -180 || long > 180) {
+      //console.log("\tLatitude: " + latmin);
+      console.log("\tLatitude: " + lat);
+      //console.log("\tLongitude: " + longmin);
+      console.log("\tLongitude: " + long);
       console.log("\tYear: " + yearmin);
       console.log("\tYear: " + yearmax);
       console.log("\tError: Invalid Longitude")
@@ -36,17 +41,23 @@ function search(latmin, longmin, latmax, longmax, yearmin, yearmax, callback) {
     }
 
     else {
-      console.log("\tLatitude: " + latmin);
-      console.log("\tLatitude: " + latmax);
-      console.log("\tLongitude: " + longmin);
-      console.log("\tLongitude: " + longmax);
+      //console.log("\tLatitude: " + latmin);
+      console.log("\tLatitude: " + lat);
+      //console.log("\tLongitude: " + longmin);
+      console.log("\tLongitude: " + long);
       console.log("\tYear: " + yearmin);
       console.log("\tYear: " + yearmax);
-    console.log("\tSuccess");
+      console.log("\tSuccess");
+
+      return db.executeQueryAsync(selStorms, [lat, 10, lat, 10, long, 10, long, 10],callback)
+      .then(function(result) {
+        return result.insertId;
+      })
+      .nodeify(callback);
 
 
    //console.log("results callback");
-  return callback (null, "Success");
+  //return callback (null, "Success");
 }
 };
 exports.search = search;
