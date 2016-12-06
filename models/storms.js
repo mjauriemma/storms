8f2acedb1570141239ca23c97e9e0cabedc1f23d
mjauriemma <matthewauriemma@gmail.com>
2016-12-06 10:48:23 -0600

    Fixed Brackets

--- models/storms.js
+++ models/storms.js
@@ -22,46 +22,19 @@ var getPoints = "Select * FROM `TyphoonX` where TyphoonNM = ?";
 
 function search(lat, long, yearmin, yearmax, callback) {
 
-    //if (latmin < -90 || latmin > 90 ||latmax <-90 || latmax > 90) {
     if (lat < -90 || lat > 90) {
-      //console.log("\tLatitude: " + latmin);
-      // console.log("\tLatitude: " + lat);
-      // //console.log("\tLongitude: " + longmin);
-      // console.log("\tLongitude: " + long);
-      // console.log("\tYear: " + yearmin);
-      // console.log("\tYear: " + yearmax);
-      // console.log("\tError: Invalid Latitude");
       return callback (new bi("Invalid Latitude"));
     }
-    // else if (longmin < -180 || longmin > 180 || longmax < -180 || longmax > 180) {
     else if (long < -180 || long > 180) {
-      //console.log("\tLatitude: " + latmin);
-      // console.log("\tLatitude: " + lat);
-      // //console.log("\tLongitude: " + longmin);
-      // console.log("\tLongitude: " + long);
-      // console.log("\tYear: " + yearmin);
-      // console.log("\tYear: " + yearmax);
-      // console.log("\tError: Invalid Longitude")
       return callback (new bi("Invalid Longitude"));
     }
-
     else {
-      //console.log("\tLatitude: " + latmin);
-      // console.log("\tLatitude: " + lat);
-      // //console.log("\tLongitude: " + longmin);
-      // console.log("\tLongitude: " + long);
-      // console.log("\tYear: " + yearmin);
-      // console.log("\tYear: " + yearmax);
-      // console.log("\tSuccess");
-
       var latmin = parseInt(lat, 10) - range;
       var latmax = parseInt(lat, 10) + range;
       var longmin = parseInt(long, 10) -range;
       var longmax = parseInt(long, 10) + range;
       yearmin = yearmin+"01-01";
       yearmax = yearmax+"12-31";
-      // console.log ("Latitude adjusted" + latmin + " " + latmax + " ")
-      // console.log(selStorms, [lat, 10, lat, 10, long, 10, long, 10])
 
       return db.executeQuery(selStorms, [yearmin, yearmax, latmin, latmax, longmin, longmax], function (err, result) {
         if (err) {
@@ -83,7 +56,8 @@ function search(lat, long, yearmin, yearmax, callback) {
 
           return callback(null, storms);
         }
-      })
+      }
+
       // .then(function(result) {
       //   console.log("result: " + result);
       //   return result;
