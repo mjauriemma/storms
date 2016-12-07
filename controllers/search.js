fde6cd576857ada3ec0598a322429480e3968ca3
mjauriemma <matthewauriemma@gmail.com>
2016-12-07 10:49:32 -0600

    Trying again

--- controllers/search.js
+++ controllers/search.js
@@ -10,16 +10,17 @@ let errors = require('../helpers/error');
 var cors = require('cors');
 let app = require('../config/app');
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 router.get('/', cors(config.cors), function (req, res)  {
     let lat = req.query.lat;
     let long = req.query.long;
-    //let latmax = req.query.latmax;
-    //let longmax = req.query.longmax;
     let yearmin = req.query.yearmin;
     let yearmax = req.query.yearmax;
-    //if (!latmin || !longmin || !latmax || !longmax) {
     if (!lat || !long){
         return webUtil.processError(res, 400, 'Latitude and Longitude must be provided', 400);
     }
@@ -31,15 +32,10 @@ router.get('/', cors(config.cors), function (req, res)  {
     }
       storms.searchAsync(lat, long, yearmin, yearmax)
           .then(response => {
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-              return res.json(response);
+            return res.json(response);
           })
           .catch(err => {
-            console.log(err);
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-              return webUtil.processError(res, 500, err.message);
+            return webUtil.processError(res, 500, err.message);
           });
 
 });
